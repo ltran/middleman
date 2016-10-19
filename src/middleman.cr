09633b79c1d94ab289ce98101660e3ebe3c81a0f
Louis Tran <tran.louis@gmail.com>
2016-10-18 23:13:05 -0700

    app.cr is a special case in crystal buildpack

--- src/middleman.cr
+++ src/middleman.cr
@@ -4,5 +4,18 @@ require "kemal"
 require "./middleman/version"
 require "./middleman/server"
 
-module Middlema
+require "option_parser"
+
+bind = "0.0.0.0"
+port = 3000
+
+OptionParser.parse! do |opts|
+  opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
+    port = opt.to_i
+  end
 end
+
+Kemal.config.port = port
+Kemal.run
+
+
