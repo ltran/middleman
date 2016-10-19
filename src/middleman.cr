2b4a044436a0740e2dc2384117fa3789e6b8ed04
Louis Tran <tran.louis@gmail.com>
2016-10-19 10:28:27 -0700

    wip

--- src/middleman.cr
+++ src/middleman.cr
@@ -1,5 +1,5 @@
-require "../libs/dotenv"
-require "../libs/kemal"
+require "dotenv"
+require "kemal"
 require "./middleman/version"
 require "./middleman/server"
 
@@ -9,16 +9,6 @@ require "option_parser"
 Dotenv.load
 
 module Middleman
-  #bind = "0.0.0.0"
-  #port = 3000
-
-  #OptionParser.parse! do |opts|
-    #opts.on("-p PORT", "--port PORT", "define port to run server") do |opt|
-      #port = opt.to_i
-    #end
-  #end
-
-  #Kemal.config.port = port
-  Kemal.run
 end
 
+Kemal.run
